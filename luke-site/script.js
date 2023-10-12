7305d60fe404dc1b91b72d673f977d9ffd3e52a7
Luke <purelyfortoontown2@gmail.com>
2023-10-12 16:10:32 +0100

    updated Taron description

--- luke-site/script.js
+++ luke-site/script.js
@@ -1,7 +1,11 @@
 let currentSlide = 0;
 const slides = document.querySelectorAll(".slideshow-image");
 const descriptionText = document.getElementById("description-text");
-const ride_descriptions = ["#5 - Ride to Happiness at Plopsaland de Panne in Belgium", "#4 - Wodan", "#3 - Red Force", "#2 - Coaster 4", "#1 - Coaster 5"];
+const ride_descriptions = ["#5 - Ride to Happiness at Plopsaland de Panne in Belgium. This ride is an Extreme Spinning coaster by manufacturer Mack, with a fast launch and fast spinning as riders go through multiple inversions", 
+                            "#4 - Wodan Timburcoaster at Europa Park in Germany. Fast paced wooden rollercoaster.", 
+                            "#3 - Red Force at Portaventura World in Spain. The tallest and fastest rollercoaster in Europe at over 100mph and 350ft tall.", 
+                            "#2 - Taron at Phantasialand in Germany. This twisty ride is surrounded by a huge rock formation giving an intense feeling of speed.", 
+                            "#1 - Coaster 5"];
 
 function showSlide(n) {
     slides[currentSlide].style.display = "none";
