f91b532cad12ff2829df506e231b9b459d22c545
Adam Brown <deepbluesea20003@gmail.com>
2023-10-12 15:53:03 +0100

    fixed if no image found

--- adam/script.js
+++ adam/script.js
@@ -1,14 +1,14 @@
 const images = {"Walter White":"https://th.bing.com/th/id/R.c37ec7b017b3ee60ca54cf3ed6d5bfd2?rik=4wFwAVhd9Se5fg&riu=http%3a%2f%2fbaltimorepostexaminer.com%2fwp-content%2fuploads%2fWalter-White_FB.jpg&ehk=8%2fCI5P6ZyQ1Zj0J1%2bGgH4zUu5H2oQztI6ixTMJ0mppg%3d&risl=&pid=ImgRaw&r=0",
 "Jesse Pinkman": "https://vignette.wikia.nocookie.net/wfl/images/5/55/Jesse_Pinkman.jpg/revision/latest?cb=20190308081818",
 "Skyler White": "https://th.bing.com/th/id/OIP.l4ALyMUd4CoUY1gcAx-kSwAAAA?pid=ImgDet&rs=1",
-"Hank Schrader": "https://example.com/hank_schrader.jpg",
+}/*"Hank Schrader": "https://example.com/hank_schrader.jpg",
 "Saul Goodman": "https://example.com/saul_goodman.jpg",
 "Mike Ehrmantraut": "https://example.com/mike_ehrmantraut.jpg",
 "Gus Fring": "https://example.com/gus_fring.jpg",
 "Jane Margolis": "https://example.com/jane_margolis.jpg",
 "Tuco Salamanca": "https://example.com/tuco_salamanca.jpg",
 "Lydia Rodarte-Quayle": "https://example.com/lydia_rodarte_quayle.jpg"
-}
+}*/
 
 window.onload = function () {
     document.getElementById('genButton').addEventListener('click', function() {
@@ -21,7 +21,10 @@ window.onload = function () {
                 document.getElementById('quote').textContent = JSON.stringify(data[0]["quote"], null, 2);
                 let author = JSON.stringify(data[0]["author"], null, 2).slice(1,-1);
                 document.getElementById('author').textContent = author;
-                document.getElementById('mainImg').src = images[author];
+                let imgLink = images[author];
+                console.log(imgLink);
+                if(imgLink===undefined){imgLink = "https://th.bing.com/th/id/OIP.m4SW8m_wQgcOX75ayigSjgHaI_?pid=ImgDet&rs=1"}
+                document.getElementById('mainImg').src = imgLink
             })
             .catch(function(error) {
                 // Handle errors
